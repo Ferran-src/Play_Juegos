1fdbedb8e3db4f8b37085bf2dea20f4953d0a54f
fercamini <ferrancamposiniesta@gmail.com>
2025-10-10 20:53:22 +0200

    Empezando el DropDownMenu

--- app/src/main/java/com/example/play_juegos/NewPlayer.kt
+++ app/src/main/java/com/example/play_juegos/NewPlayer.kt
@@ -7,7 +7,7 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.rememberScrollState
@@ -15,6 +15,8 @@ import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.Button
 import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.DropdownMenu
+import androidx.compose.material3.DropdownMenuItem
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextField
@@ -27,18 +29,19 @@ import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.innerShadow
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.layout.ModifierLocalBeyondBoundsLayout
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
 
 @Composable
-fun InterfaceNewPlayer(modifier: Modifier){
+fun InterfaceNewPlayer(){
     val rowAligment = 64.dp
     var campoNombre by remember {  mutableStateOf(false)}
     var campoNickname by remember {  mutableStateOf(false)}
+
+    var expanded by remember { mutableStateOf(false) }
+    val series = listOf("Naruto", "The witcher", "The expanse", "The walking dead", "Stranger Things")
 
     var nombreTextField by rememberSaveable { mutableStateOf(" ") }
     var apellidoTextField by rememberSaveable { mutableStateOf(" ") }
@@ -56,8 +59,8 @@ fun InterfaceNewPlayer(modifier: Modifier){
         verticalArrangement = Arrangement.spacedBy(40.dp)
 
     ){
-        Column {
-            Row() {
+        Column{
+            Row {
                 Image(
                     modifier = Modifier
                         .size(64.dp)
@@ -103,7 +106,7 @@ fun InterfaceNewPlayer(modifier: Modifier){
         }
 
 
-        Row() {
+        Row {
             Spacer(modifier = modifier.size(rowAligment,0.dp))
 
             Image(
@@ -126,7 +129,7 @@ fun InterfaceNewPlayer(modifier: Modifier){
                 Text(stringResource(R.string.Change))
             }
         }
-        Row() {
+        Row{
             Image(
                 modifier = modifier
                     .size(64.dp)
@@ -142,7 +145,7 @@ fun InterfaceNewPlayer(modifier: Modifier){
                 onValueChange ={phoneTextField = it})
 
         }
-        Row() {
+        Row {
             Image(
                 modifier = modifier
                     .size(64.dp)
@@ -195,4 +198,58 @@ fun CreateTextField(variable: String, texto: String,
             focusedIndicatorColor = MaterialTheme.colorScheme.tertiary,
             unfocusedIndicatorColor = Color.Transparent)
     )
+}
+@Composable
+fun CreateMenuTextField(variable: String, texto: String,
+                     modifier: Modifier,
+                     onValueChange: (String) -> Unit,
+                        expanded: Boolean,
+                        onExpandedChange: (Boolean)-> Unit,
+                        series: List<String>,
+                        onClickExpandedChange: (String, Boolean) -> Unit){
+     Column {
+         TextField(
+             modifier = modifier.padding(vertical = 2.dp),
+             enabled = false,
+             value = variable,
+             onValueChange = onValueChange,
+             label = {
+                 Text(
+                     text = texto,
+                     color = Color.Black
+                 )
+             },
+             shape = RoundedCornerShape(
+                 topStart = 16.dp,
+                 topEnd = 16.dp,
+                 bottomStart = 0.dp,
+                 bottomEnd = 0.dp
+             ),
+             colors = TextFieldDefaults.colors(
+                 unfocusedContainerColor = MaterialTheme.colorScheme.secondary,
+                 focusedContainerColor = MaterialTheme.colorScheme.secondary,
+                 focusedIndicatorColor = MaterialTheme.colorScheme.tertiary,
+                 unfocusedIndicatorColor = Color.Transparent
+             )
+         )
+         DropdownMenu(
+             expanded = expanded,
+             onDismissRequest = { onExpandedChange /*expanded = false*/ },
+             modifier = Modifier.fillMaxWidth()
+         ) {
+             series.forEach { serie ->
+                 DropdownMenuItem(
+                     text = {Text(text = serie)},
+                             onClick = {
+                         onClickExpandedChange
+                         /*
+                             expanded = false
+                             variable = serie
+
+                          */
+                         }
+                 )
+             }
+         }
+     }
 }
